9edf570987b2a7d8f97bfc334d34c62da307e952
James Farrell <james@toucan.earth>
2024-04-24 20:26:15 +0200

    merge: results of stash

--- packages/foundry/contracts/HyperStaking.sol
+++ packages/foundry/contracts/HyperStaking.sol
@@ -5,16 +5,18 @@ pragma solidity 0.8.20;
 import "forge-std/console.sol";
 import {IHypercertToken} from "hypercerts-contracts/interfaces/IHypercertToken.sol";
 
-// Use openzeppelin to inherit battle-tested implementations (ERC20, ERC721, etc)
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/utils/ReentrancyGuard.sol";
 
 /**
  * A smart contract that allows changing a state variable of the contract and tracking the changes
  * It also allows the owner to withdraw the Ether in the contract
  * @author BuidlGuidl
  */
-contract HyperStaking {
+contract HyperStaking is ReentrancyGuard {
     IHypercertToken public immutable stakingToken;
+
+    // Mapping from user address to mapping of token ID to Stake details
+    mapping(address => mapping(uint256 => Stake)) public stakes;
 
     // Events: a way to emit log statements from smart contract that can be listened to by external parties
     event Stake(address staker, uint256 amountStaked);
@@ -23,5 +25,31 @@ contract HyperStaking {
     // Check packages/foundry/script/Deploy.s.sol
     constructor(address _stakingToken) {
         stakingToken = IHypercertToken(_stakingToken);
+    }
+
+    function stake(uint256 id, uint256 amount) external nonReentrant {
+        require(amount > 0, "Amount must be greater than 0");
+        require(
+            stakingToken.balanceOf(msg.sender, id) >= amount,
+            "Insufficient balance"
+        );
+
+        // Transfer the tokens to this contract for staking
+        stakingToken.safeTransferFrom(
+            msg.sender,
+            address(this),
+            id,
+            amount,
+            ""
+        );
+
+        // Update the stake details
+        // stakes[msg.sender][id].amount += amount;
+        // stakes[msg.sender][id].timestamp = block.timestamp;
+
+        // // Add the token ID to the user's staked tokens set
+        // _stakedTokens[msg.sender].add(id);
+
+        // emit Staked(msg.sender, id, amount, block.timestamp);
     }
 }
