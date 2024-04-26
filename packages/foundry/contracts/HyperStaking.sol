4c16118921a44a8a4227b24192efeece724194bf
James Farrell <james@toucan.earth>
2024-04-26 20:58:38 +0200

    feat: first basic stake with tests

--- packages/foundry/contracts/HyperStaking.sol
+++ packages/foundry/contracts/HyperStaking.sol
@@ -3,32 +3,56 @@ pragma solidity 0.8.20;
 
 // Useful for debugging. Remove when deploying to a live network.
 import "forge-std/console.sol";
-import {IHypercertToken} from "hypercerts-contracts/interfaces/IHypercertToken.sol";
+import {ERC1155Upgradeable} from "oz-upgradeable/token/ERC1155/ERC1155Upgradeable.sol";
 
 import "@openzeppelin/utils/ReentrancyGuard.sol";
+import "@openzeppelin/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/utils/structs/EnumerableSet.sol";
 
 /**
  * A smart contract that allows changing a state variable of the contract and tracking the changes
  * It also allows the owner to withdraw the Ether in the contract
  * @author BuidlGuidl
  */
-contract HyperStaking is ReentrancyGuard {
-    IHypercertToken public immutable stakingToken;
+contract HyperStaking is ReentrancyGuard, ERC1155Holder {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    ERC1155Upgradeable public immutable stakingToken;
 
     // Mapping from user address to mapping of token ID to Stake details
     mapping(address => mapping(uint256 => Stake)) public stakes;
 
+    // Mapping to keep track of which token IDs each address has staked
+    mapping(address => EnumerableSet.UintSet) private _stakedTokens;
+
+    struct Stake {
+        uint256 amount;
+        uint256 timestamp;
+    }
+
     // Events: a way to emit log statements from smart contract that can be listened to by external parties
-    event Stake(address staker, uint256 amountStaked);
+    event Staked(
+        address indexed user,
+        uint256 indexed tokenId,
+        uint256 amount,
+        uint256 timestamp
+    );
+    event Unstaked(
+        address indexed user,
+        uint256 indexed tokenId,
+        uint256 amount,
+        uint256 timestamp
+    );
 
     // Constructor: Called once on contract deployment
     // Check packages/foundry/script/Deploy.s.sol
     constructor(address _stakingToken) {
-        stakingToken = IHypercertToken(_stakingToken);
+        stakingToken = ERC1155Upgradeable(_stakingToken);
     }
 
     function stake(uint256 id, uint256 amount) external nonReentrant {
         require(amount > 0, "Amount must be greater than 0");
+        console.log("In Stake function", msg.sender, id, amount);
         require(
             stakingToken.balanceOf(msg.sender, id) >= amount,
             "Insufficient balance"
@@ -44,12 +68,20 @@ contract HyperStaking is ReentrancyGuard {
         );
 
         // Update the stake details
-        // stakes[msg.sender][id].amount += amount;
-        // stakes[msg.sender][id].timestamp = block.timestamp;
+        stakes[msg.sender][id].amount += amount;
+        stakes[msg.sender][id].timestamp = block.timestamp;
 
         // // Add the token ID to the user's staked tokens set
-        // _stakedTokens[msg.sender].add(id);
+        _stakedTokens[msg.sender].add(id);
 
-        // emit Staked(msg.sender, id, amount, block.timestamp);
+        emit Staked(msg.sender, id, amount, block.timestamp);
+    }
+
+    // Rewritten function to return the staked amount for a given tokenId and user
+    function stakedAmount(
+        address user,
+        uint256 tokenId
+    ) external view returns (uint256) {
+        return stakes[user][tokenId].amount;
     }
 }
--- packages/foundry/test/HyperStaking.t.sol
+++ packages/foundry/test/HyperStaking.t.sol
@@ -1,31 +1,81 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.17;
+pragma solidity ^0.8.20;
 
 import "forge-std/Test.sol";
 import "../contracts/HyperStaking.sol";
 import "hypercerts-contracts/HypercertMinter.sol";
+import {IHypercertToken} from "hypercerts-contracts/interfaces/IHypercertToken.sol";
 
 contract HyperStakingTest is Test {
     HypercertMinter public hypercertMinter;
-    HyperStaking public hyperStaking;
+    HyperStaking public stakingContract;
+    address public testUser;
+    ERC1155Upgradeable public tokenContract;
+    string internal _uri;
+    event ClaimStored(uint256 indexed claimID, string uri, uint256 totalUnits);
+    uint256 public testClaimId;
 
     function setUp() public {
         hypercertMinter = new HypercertMinter();
-        //hyperStaking = new HyperStaking();
+        stakingContract = new HyperStaking(address(hypercertMinter));
+        testUser = vm.addr(1);
+        tokenContract = ERC1155Upgradeable(address(hypercertMinter));
+
+        // Mint tokens to the testUser
+        uint256 units = 100;
+        _uri = "ipfs://bafybeigdyrzt5sfp7udm7hu76uh7y26nf3efuylqabf3oclgtqy55fbzdi";
+        // Setup expectations for the event emission
+        vm.startPrank(testUser);
+        vm.recordLogs(); // Start recording logs
+
+        vm.expectEmit(false, false, false, true);
+        emit ClaimStored(0x100000000000000000000000000000001, _uri, units); // Adjust this line if more parameters need to match exactly
+
+        hypercertMinter.mintClaim(
+            testUser,
+            units,
+            _uri,
+            IHypercertToken.TransferRestrictions.AllowAll
+        );
+
+        vm.stopPrank();
+
+        // Retrieve and decode logs to capture claimID
+        Vm.Log[] memory logs = vm.getRecordedLogs();
+        uint256 capturedClaimID;
+        bool foundClaimID = false;
+
+        for (uint256 i = 0; i < logs.length; i++) {
+            if (
+                logs[i].topics[0] ==
+                keccak256("ClaimStored(uint256,string,uint256)")
+            ) {
+                capturedClaimID = uint256(logs[i].topics[1]);
+                console.log("ClaimStored: claimID found", capturedClaimID);
+
+                foundClaimID = true;
+                break;
+            }
+        }
+
+        testClaimId = capturedClaimID;
     }
 
-    function testMessageOnDeployment() public view {
-        // require(
-        //     keccak256(bytes(yourContract.greeting())) ==
-        //         keccak256("Building Unstoppable Apps!!!")
-        // );
-    }
+    function testStake() public {
+        vm.prank(testUser);
+        uint256 balance = hypercertMinter.balanceOf(testUser, testClaimId);
 
-    function testSetNewMessage() public {
-        // yourContract.setGreeting("Learn Scaffold-ETH 2! :)");
-        // require(
-        //     keccak256(bytes(yourContract.greeting())) ==
-        //         keccak256("Learn Scaffold-ETH 2! :)")
-        // );
+        vm.prank(testUser);
+        tokenContract.setApprovalForAll(address(stakingContract), true);
+        vm.prank(testUser);
+        stakingContract.stake(testClaimId, balance);
+
+        uint256 stakedAmount = stakingContract.stakedAmount(
+            testUser,
+            testClaimId
+        );
+        vm.stopPrank();
+
+        assertEq(stakedAmount, balance, "not equal");
     }
 }
